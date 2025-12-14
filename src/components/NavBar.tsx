0678b0e27b95c379caec1ae35110a2f584fbf941
mhsanjunaid-dot <mhsanjunaid@gmail.com>
2025-12-15 03:42:56 +0530

    Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
 const NavBar = () => { 
 return (
-<HStack justifyContent={"space-between"} padding={'10px'}>
+<HStack  padding={'10px'}>
 <Image src={logo} boxSize={'60px'} />
 <ColorModeSwitch/>
 </HStack>
